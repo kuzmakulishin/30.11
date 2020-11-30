bfadcd72983aa402ec5abe2ad6af6aa1529ce5a1
Kuzma Kulishin <kuzma.zzz@gmail.com>
2020-11-30 20:37:38 +0300

    Initial Commit

---
+++ 30.11/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  30.11
+//
+//  Created by Kuzma Kulishin on 30.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
